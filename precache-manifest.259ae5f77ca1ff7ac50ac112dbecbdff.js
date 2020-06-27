4e8411a907b8c83123e2c79779c0de2e186aac7c
RyanJMS <59546790+RyanJMS@users.noreply.github.com>
2020-06-27 09:57:53 -0400

    Updates

---
+++ precache-manifest.259ae5f77ca1ff7ac50ac112dbecbdff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cdb0d6ec57a7bffc6607db38f909766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e22bfceabb8f187c6dd",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7d3d48a9a2de0df6b109",
+    "url": "/static/css/main.53b77fdf.chunk.css"
+  },
+  {
+    "revision": "8e22bfceabb8f187c6dd",
+    "url": "/static/js/2.6cc46e13.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cc46e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3d48a9a2de0df6b109",
+    "url": "/static/js/main.85e54056.chunk.js"
+  },
+  {
+    "revision": "7a235cb05d8669b2d22d",
+    "url": "/static/js/runtime-main.7ba75982.js"
+  }
+]);
